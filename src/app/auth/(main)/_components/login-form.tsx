4603aed2b8614857109fd5945675f38aebff3701
Sammy Ferreira <sammyfe2021@gmail.com>
2024-04-17 21:56:26 -0300

    feat: data login form

--- src/app/auth/(main)/_components/login-form.tsx
+++ src/app/auth/(main)/_components/login-form.tsx
@@ -1,14 +1,33 @@
+'use client'
+
 import EmailIcon from '@/components/icons/EmailIcon'
 import PasswordIcon from '@/components/icons/PasswordIcon'
 import { Input } from '@/components/ui/input'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { useForm } from 'react-hook-form'
+import { loginSchema } from '../schema'
+import { zodResolver } from '@hookform/resolvers/zod'
 
 const LoginForm = () => {
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const handleSubmit = form.handleSubmit(async (data) => {
+    try {
+      setTimeout(() => {
+        console.log(data)
+      }, 100)
+    } catch (error) {
+      console.log(error)
+    }
+  })
+
   return (
     <>
-      <div className="grid w-full gap-8">
+      <form onSubmit={handleSubmit} id="login" className="grid w-full gap-8">
         <div className="flex flex-row items-center justify-center gap-2">
           <EmailIcon />
           <Input
@@ -18,6 +37,7 @@ const LoginForm = () => {
             required
             type="email"
             autoComplete="off"
+            {...form.register('email')}
           />
         </div>
         <div className="grid gap-2">
@@ -30,19 +50,22 @@ const LoginForm = () => {
               required
               type="password"
               autoComplete="off"
+              {...form.register('password')}
             />
           </div>
           <Link className="ml-auto inline-block text-xs underline" href="#">
             Esqueceu sua senha?
           </Link>
         </div>
-      </div>
+      </form>
       <div className="mx-16">
         <Button
+          form="login"
           className="my-4 w-full bg-emerald-600 font-light text-emerald-50 shadow-[6.0px_10.0px_10.0px_rgba(0,0,0,0.38)] transition-colors hover:bg-emerald-500 hover:shadow-[3.0px_6.0px_6.0px_rgba(0,0,0,0.38)]"
           type="submit"
+          disabled={form.formState.isSubmitting}
         >
-          Login
+          {form.formState.isSubmitting ? 'Logando...' : 'Login'}
         </Button>
       </div>
     </>
--- src/app/auth/(main)/page.tsx
+++ src/app/auth/(main)/page.tsx
@@ -2,6 +2,7 @@ import LoginBanner from './_components/login-banner'
 import LoginAbout from './_components/login-about'
 import LoginForm from './_components/login-form'
 import LoginFormExtra from './_components/login-form-extra'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 
 export default function Auth() {
   return (
@@ -11,13 +12,15 @@ export default function Auth() {
         <LoginAbout />
       </div>
       <div className="flex items-center justify-center py-12 text-white">
-        <div className="mx-auto grid w-[375px] rounded-2xl bg-verdee px-8 py-6">
-          <div className="grid w-full gap-2 py-6 text-center">
+        <Card className="mx-auto grid w-[375px] rounded-2xl bg-verdee px-8 py-6">
+          <CardHeader className="grid w-full gap-2 py-6 text-center text-white">
             <h1 className="my-2 text-3xl font-bold">LOGIN</h1>
-          </div>
-          <LoginForm />
-          <LoginFormExtra />
-        </div>
+          </CardHeader>
+          <CardContent className="p-0 text-white">
+            <LoginForm />
+            <LoginFormExtra />
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
