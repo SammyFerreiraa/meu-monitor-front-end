059185d9b2460c29a0fd099dc2848f883fb2e6ae
Sammy Ferreira <sammyfe2021@gmail.com>
2024-04-18 14:50:57 -0300

    fear: icon and images

---
+++ src/components/icons/EmailGreenIcon.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+const EmailGreenIcon = () => (
+  <svg
+    width="31"
+    height="33"
+    viewBox="0 0 37 33"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d_415_550)">
+      <g>
+        <path
+          d="M23.0977 24.5H9.69924C5.6797 24.5 3 22.4706 3 17.7353V8.26471C3 3.52941 5.6797 1.5 9.69924 1.5H23.0977C27.1173 1.5 29.797 3.52941 29.797 8.26471V17.7353C29.797 22.4706 27.1173 24.5 23.0977 24.5Z"
+          stroke="#0C5149"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M23.0977 8.94104L18.904 12.3234C17.5239 13.4328 15.2596 13.4328 13.8795 12.3234L9.69922 8.94104"
+          stroke="#0C5149"
+          strokeWidth="3"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_415_550"
+        x="0.5"
+        y="0"
+        width="35.797"
+        height="33"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="2" dy="4" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_415_550"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_415_550"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+)
+
+export default EmailGreenIcon
---
+++ src/components/icons/UserIcon.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const UserIcon = () => (
+  <svg
+    width="33"
+    height="37"
+    viewBox="0 0 33 37"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d_415_556)">
+      <path
+        d="M14.2611 14.6425C18.172 14.6425 21.3426 11.441 21.3426 7.49176C21.3426 3.54255 18.172 0.341064 14.2611 0.341064C10.3501 0.341064 7.17957 3.54255 7.17957 7.49176C7.17957 11.441 10.3501 14.6425 14.2611 14.6425Z"
+        fill="#0C5149"
+      />
+      <path
+        d="M14.2613 18.2178C7.16559 18.2178 1.38708 23.023 1.38708 28.9438C1.38708 29.3443 1.69867 29.6589 2.09523 29.6589H26.4273C26.8239 29.6589 27.1355 29.3443 27.1355 28.9438C27.1355 23.023 21.357 18.2178 14.2613 18.2178Z"
+        fill="#0C5149"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_415_556"
+        x="0.387085"
+        y="0.341064"
+        width="31.7484"
+        height="36.3179"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="2" dy="4" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_415_556"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_415_556"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+)
+
+export default UserIcon
