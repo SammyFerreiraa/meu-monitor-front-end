e70c50eb7a44815d924c1faf638390e8e453c967
Sammy Ferreira <sammyfe2021@gmail.com>
2024-04-18 21:37:25 -0300

    refactor: icons

---
+++ src/components/icons/TokenIcon.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const TokenIcon = () => (
+  <svg
+    width="34"
+    height="34"
+    viewBox="0 0 31 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g id="icon-outline-token" filter="url(#filter0_d_415_581)">
+      <path
+        id="Vector"
+        d="M23.8333 7.04159L13 1.08325L2.16663 7.04159V18.9583L13 24.9166L23.8333 18.9583V7.04159ZM13 3.81217L20.1138 7.72084L16.4907 9.71092C15.612 8.80525 14.3722 8.23325 13 8.23325C11.6277 8.23325 10.3879 8.80525 9.50922 9.71092L5.88607 7.72084L13 3.81217ZM11.7963 21.5323L4.57403 17.564V9.73475L8.34163 11.8083C8.23329 12.1777 8.18514 12.5828 8.18514 12.9999C8.18514 15.2164 9.71385 17.0873 11.7963 17.6117V21.5323ZM10.5926 12.9999C10.5926 11.6891 11.6759 10.6166 13 10.6166C14.324 10.6166 15.4074 11.6891 15.4074 12.9999C15.4074 14.3108 14.324 15.3833 13 15.3833C11.6759 15.3833 10.5926 14.3108 10.5926 12.9999ZM14.2037 21.5323V17.6236C16.2861 17.0993 17.8148 15.2283 17.8148 13.0118C17.8148 12.5948 17.7666 12.1896 17.6583 11.8083L21.4259 9.73475V17.564L14.2037 21.5323Z"
+        fill="#0C5149"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d_415_581"
+        x="0"
+        y="0"
+        width="32"
+        height="34"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="2" dy="4" />
+        <feGaussianBlur stdDeviation="1.5" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.2 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_415_581"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_415_581"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+)
+
+export default TokenIcon
